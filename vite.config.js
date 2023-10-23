a69da2fb31438c00ab9e3f04131b25f0bcd4fa43
rocnail23 <jdanielrojas16@gmail.com>
2023-10-23 19:46:04 -0400

    l

--- vite.config.js
+++ vite.config.js
@@ -7,38 +7,23 @@ export default defineConfig({
     plugins: [
         react(),
         VitePWA({
-            // add this to cache all the imports
-            workbox: {
-                globPatterns: ["**/*"],
-            },
-            // add this to cache all the
-            // static assets in the public folder
-            includeAssets: [
-                "**/*",
-            ],
+           strategies:"injectManifest",
+           srcDir:"src",
+           filename:"my-sw.js",
+
             manifest: {
                 "theme_color": "#f69435",
                 "background_color": "#f69435",
                 "display": "standalone",
                 "scope": "/",
                 "start_url": "/",
-                "short_name": "Vite PWA",
-                "description": "Vite PWA Demo",
-                "name": "Vite PWA",
+                "short_name": "calendy",
+                "description": "hey install calendy",
+                "name": "may calendy today",
                 "icons": [
                     {
                         "src": "/icon-192x192.png",
                         "sizes": "192x192",
-                        "type": "image/png"
-                    },
-                    {
-                        "src": "/icon-256x256.png",
-                        "sizes": "256x256",
-                        "type": "image/png"
-                    },
-                    {
-                        "src": "/icon-384x384.png",
-                        "sizes": "384x384",
                         "type": "image/png"
                     },
                     {
