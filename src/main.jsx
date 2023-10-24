c494dae78334a682246193e1447827610028aad2
rocnail23 <jdanielrojas16@gmail.com>
2023-10-23 20:17:15 -0400

    w

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,7 @@ import ReactDOM from 'react-dom/client'
 import CalendarApp from './CalendarApp.jsx'
 import { registerSW } from "virtual:pwa-register";
 
-const updateSW = registerSW({
-  onNeedRefresh() {
-    if (confirm("New content available. Reload?")) {
-      updateSW(true);
-    }
-  },
-  onOfflineReady() {
-    console.log("offline ready");
-  },
-});
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
