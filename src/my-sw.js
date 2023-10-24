f19815e7dbf586ce3cb8b1a903d43367e2cad7c0
rocnail23 <jdanielrojas16@gmail.com>
2023-10-23 22:15:46 -0400

    l

--- src/my-sw.js
+++ src/my-sw.js
@@ -7,26 +7,19 @@ import {precacheAndRoute} from 'workbox-precaching';
 precacheAndRoute(self.__WB_MANIFEST);
 
 // Enable navigation preload
-navigationPreload.enable();
+
 
 // Create a new navigation route that uses the Network-first, falling back to
 // cache strategy for navigation requests with its own cache. This route will be
 // handled by navigation preload. The NetworkOnly strategy will work as well.
-const navigationRoute = new NavigationRoute(new NetworkFirst({
-  cacheName: 'navigations'
-}));
+
 
 // Register the navigation route
-registerRoute(navigationRoute);
+
 
 // Create a route for image, script, or style requests that use a
 // stale-while-revalidate strategy. This route will be unaffected
 // by navigation preload.
-const staticAssetsRoute = new Route(({request}) => {
-  return ['image', 'script', 'style'].includes(request.destination);
-}, new StaleWhileRevalidate({
-  cacheName: 'static-assets'
-}));
+
 
 // Register the route handling static assets
-registerRoute(staticAssetsRoute);
